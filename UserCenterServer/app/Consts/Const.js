c3e19f8f949229ea8b73b6f97937d7d6903136ae
GeforceLee <liyunlong880601@gmail.com>
2013-03-23 16:48:17 +0800

    连接connector  验证

--- UserCenterServer/app/Consts/Const.js
+++ UserCenterServer/app/Consts/Const.js
@@ -10,13 +10,15 @@ module.exports = {
     OK: 200,
     FAIL: 500,
     ErrorCode: {
-        SERVER_ERROR:-1,
+        SERVER_ERROR: -1,
         PASSWORD_ERROR: 0,
         NO_USERNAME: 1,
         ALREADY_HAS_USER: 2,
         ALREADY_LOGIN: 3,
         PARAM_ERROR: 4,
-        GUEST_FORBIDDEN:5
+        GUEST_FORBIDDEN: 5,
+        AUTH_FILED: 6,
+        AUTH_TIMEOUT: 7
     },
     UserRole: {
         DEFAULT: 0,
